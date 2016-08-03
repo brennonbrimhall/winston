e3cb125f57753734824fcb546d05f74b9aafa24e
Taylor Smith <taviarsokin@gmail.com>
2016-08-03 14:41:55 -0600

    Added ability for Winston to text you its IP address, with some attemt to keep people from accidentally getting it.

--- app.js
+++ app.js
@@ -47,140 +47,162 @@ n.on('end', function(){
 	//Verify that this is a Text Message coming in as an email.  We
 	//will do that by seeing if the first three characters in the
 	//subject line reads "SMS"
+
+	var mailer = require('./helpers/mailer.js');
+
 	if(mail.subject.substring(0, 3) === 'SMS'){
 		//Then we're good!
 
-		//If it is Monday, accept the text:
-		if((new Date()).getDay() === 1){
-			var phoneNumber = mail.from[0].name;
-			
-			//Formatting the string to match up with iMOS:
-			//First, removing the space and adding a dash
-			phoneNumber = phoneNumber.replace(' ', '-');
-			
-			//Now, removing parenthesees:
-			phoneNumber = phoneNumber.replace('(', '');
-			phoneNumber = phoneNumber.replace(')', '');
-			
-			//Now, adding a +1:
-			phoneNumber = '+1 '+phoneNumber;
-			//Finished!
+		var getIPCommands = [
+			"ip ",
+			"ip?"
+		];
 
-			//Now, parsing their numbers
-			var text = mail.text;
-			var report = {};
-			text = text.split('\n'); //Splitting into array based off of newline
+		var ipCommand = false;
 
-			console.log(text);
-			
-			text.forEach(function(currentValue, index, array){
-				if(currentValue.split(':').length == 2){
-					//If we have a line with no semicolons or too many, ignore.
-					var indicator = currentValue.split(':')[0].toLowerCase();
-					var value = parseInt(currentValue.split(':')[1]);
-					report[indicator] = value;
-				}else{
-					console.log('Too little/many semicolons')
-				}
+		for(var i = 0; i < getIPCommands.length; i++){
+			if(mail.text.toLowerCase().includes(getIPCommands[i])){
+				ipCommand = true;
+			}
+		}
+
+		if(ipCommand){
+			console.log('Giving public IP address to '+mail.from[0].name);
+			var ipify = require('ipify');
+			ipify((err, ip) => {
+				console.log('    - ' + ip);
+				mailer.mail(mail.from[0].address, '', 'I live at '+ip+' currently.  Love, Winston');
 			});
+		}else{
 
-			//Validate that we got all the indicators we wanted
-			var dynamicRequire = require('./helpers/dynamicRequire.js');
-			console.log('Required dynamicRequire:'+dynamicRequire);
-			var reportConfig = dynamicRequire.read('../weeklyreports/config.json');
+			//If it is Monday, accept the text:
+			if((new Date()).getDay() === 1){
+				var phoneNumber = mail.from[0].name;
+				
+				//Formatting the string to match up with iMOS:
+				//First, removing the space and adding a dash
+				phoneNumber = phoneNumber.replace(' ', '-');
+				
+				//Now, removing parenthesees:
+				phoneNumber = phoneNumber.replace('(', '');
+				phoneNumber = phoneNumber.replace(')', '');
+				
+				//Now, adding a +1:
+				phoneNumber = '+1 '+phoneNumber;
+				//Finished!
 
-			//Add in miles (it's special and so we don't want it in config)
-			reportConfig.push({
-				"name": "Miles",
-				"shortname": "miles"
-			});
-			var valid = true;
-			var indicatorsMissing=[];
+				//Now, parsing their numbers
+				var text = mail.text;
+				var report = {};
+				text = text.split('\n'); //Splitting into array based off of newline
 
-			console.dir(report);
-
-			reportConfig.forEach(function(currentValue, index, array){
-				//Do we have a numerical value for each of the indicators in the config?
-				if(typeof report[currentValue.shortname] === 'undefined' || isNaN(report[currentValue.shortname])){
-					valid = false;
-					indicatorsMissing.push(currentValue.shortname);
-					console.log('Report invalid');
-				}
-			});
-
-			//If valid, save and say that it was successfully inputted.
-			//If not, reply that there was a problem.
-			if(valid){
-				//Save file
-				var today = new Date();
-
-				var numbers;
-				try{
-					//Get the file that holds all of this week's numbers reports
-					numbers = dynamicRequire.readWeeklyReport(today);
-					console.log('Was able to load up previous report file.');
-				}catch(err){
-					//Init empty array.  We will create this file.
-					console.log(err);
-					numbers = [];
-				}
-
-				//Adding it to the numbers reports
-
-				//Do we have a record already from them today?  If so, we need to overwrite it.
-				var needToOverwrite = false;
-				var overwriteIndex;
-
-				console.log(numbers);
-
-				numbers.forEach(function(currentValue, index, array){
-					if(currentValue.phone == phoneNumber){
-						needToOverwrite = true;
-						overwriteIndex = index;
+				console.log(text);
+				
+				text.forEach(function(currentValue, index, array){
+					if(currentValue.split(':').length == 2){
+						//If we have a line with no semicolons or too many, ignore.
+						var indicator = currentValue.split(':')[0].toLowerCase();
+						var value = parseInt(currentValue.split(':')[1]);
+						report[indicator] = value;
+					}else{
+						console.log('Too little/many semicolons')
 					}
 				});
 
-				if(needToOverwrite){
-					//Overwriting.
-					numbers[overwriteIndex] = {recieved: Date.now(), phone: phoneNumber, report: report};
-					console.log('Overwriting')
-				}else{
-					//No need to overwrite, appending.
-					numbers.push({recieved: Date.now(), phone: phoneNumber, report: report});
-					console.log('Saving')
-				}
+				//Validate that we got all the indicators we wanted
+				var dynamicRequire = require('./helpers/dynamicRequire.js');
+				console.log('Required dynamicRequire:'+dynamicRequire);
+				var reportConfig = dynamicRequire.read('../weeklyreports/config.json');
 
-				dynamicRequire.writeWeeklyReport(today, numbers);
+				//Add in miles (it's special and so we don't want it in config)
+				reportConfig.push({
+					"name": "Miles",
+					"shortname": "miles"
+				});
+				var valid = true;
+				var indicatorsMissing=[];
 
-				var mailer = require('./helpers/mailer.js');
+				console.dir(report);
 
-				if(needToOverwrite){
-					mailer.mail(mail.from[0].address, '', 'Thank you for submitting your numbers report again.  I was able to successfully process it and overwrite your previous report.  Be sure to submit your report again through the missionary portal.  Love, Winston');
-					console.log('Sent acknowledgement text for overwrite');
-				}else{
-					if((new Date).getHours() > 9){
-						mailer.mail(mail.from[0].address, '', 'My dear missionary, you are LATE with your report -- you must submit it after the mission prayer and before 9:15!  I did, however, process your report.  Be sure to submit your report again through the missionary portal.  Love, Winston');	
-						console.log('Sent acknowledgement text');
-					}else if ((new Date).getHours() < 9){
-						mailer.mail(mail.from[0].address, '', 'My dear missionary, you are EARLY with your report -- you must submit it after the mission prayer and before 9:15!  I did, however, process your report.  Be sure to submit your report again through the missionary portal.  Love, Winston');	
-						console.log('Sent acknowledgement text');
-					}else{
-						mailer.mail(mail.from[0].address, '', 'Thank you for your numbers report.  Be sure to submit your report again through the missionary portal.  Love, Winston');	
-						console.log('Sent acknowledgement text');
+				reportConfig.forEach(function(currentValue, index, array){
+					//Do we have a numerical value for each of the indicators in the config?
+					if(typeof report[currentValue.shortname] === 'undefined' || isNaN(report[currentValue.shortname])){
+						valid = false;
+						indicatorsMissing.push(currentValue.shortname);
+						console.log('Report invalid');
 					}
+				});
+
+				//If valid, save and say that it was successfully inputted.
+				//If not, reply that there was a problem.
+				if(valid){
+					//Save file
+					var today = new Date();
+
+					var numbers;
+					try{
+						//Get the file that holds all of this week's numbers reports
+						numbers = dynamicRequire.readWeeklyReport(today);
+						console.log('Was able to load up previous report file.');
+					}catch(err){
+						//Init empty array.  We will create this file.
+						console.log(err);
+						numbers = [];
+					}
+
+					//Adding it to the numbers reports
+
+					//Do we have a record already from them today?  If so, we need to overwrite it.
+					var needToOverwrite = false;
+					var overwriteIndex;
+
+					console.log(numbers);
+
+					numbers.forEach(function(currentValue, index, array){
+						if(currentValue.phone == phoneNumber){
+							needToOverwrite = true;
+							overwriteIndex = index;
+						}
+					});
+
+					if(needToOverwrite){
+						//Overwriting.
+						numbers[overwriteIndex] = {recieved: Date.now(), phone: phoneNumber, report: report};
+						console.log('Overwriting')
+					}else{
+						//No need to overwrite, appending.
+						numbers.push({recieved: Date.now(), phone: phoneNumber, report: report});
+						console.log('Saving')
+					}
+
+					dynamicRequire.writeWeeklyReport(today, numbers);
+
+					if(needToOverwrite){
+						mailer.mail(mail.from[0].address, '', 'Thank you for submitting your numbers report again.  I was able to successfully process it and overwrite your previous report.  Be sure to submit your report again through the missionary portal.  Love, Winston');
+						console.log('Sent acknowledgement text for overwrite');
+					}else{
+						if((new Date).getHours() > 9){
+							mailer.mail(mail.from[0].address, '', 'My dear missionary, you are LATE with your report -- you must submit it after the mission prayer and before 9:15!  I did, however, process your report.  Be sure to submit your report again through the missionary portal.  Love, Winston');	
+							console.log('Sent acknowledgement text');
+						}else if ((new Date).getHours() < 9){
+							mailer.mail(mail.from[0].address, '', 'My dear missionary, you are EARLY with your report -- you must submit it after the mission prayer and before 9:15!  I did, however, process your report.  Be sure to submit your report again through the missionary portal.  Love, Winston');	
+							console.log('Sent acknowledgement text');
+						}else{
+							mailer.mail(mail.from[0].address, '', 'Thank you for your numbers report.  Be sure to submit your report again through the missionary portal.  Love, Winston');	
+							console.log('Sent acknowledgement text');
+						}
+					}
+
+				}else{
+					//Reply that there was a problem.
+					mailer.mail(mail.from[0].address, '', 'Hmmm...it seems we\'re missing the following indicators: '+JSON.stringify(indicatorsMissing)+'.  Can you try sending your report again?  Love, Winston');
 				}
 
 			}else{
-				//Reply that there was a problem.
-				var mailer = require('./helpers/mailer.js');
-				mailer.mail(mail.from[0].address, '', 'Hmmm...it seems we\'re missing the following indicators: '+JSON.stringify(indicatorsMissing)+'.  Can you try sending your report again?  Love, Winston');
+				//It's not Monday, so we need to reject the text.
+				console.log('Report recieved, even though it\'s not Monday.');
+				mailer.mail(mail.from[0].address, '', 'Perhaps we have a misunderstanding...it\'s not Monday.  If you really do need to send this in now, please contact the Assistants at 303-929-1845.  Love, Winston');
 			}
-
-		}else{
-			//It's not Monday, so we need to reject the text.
-			console.log('Report recieved, even though it\'s not Monday.')
-			var mailer = require('./helpers/mailer.js');
-			mailer.mail(mail.from[0].address, '', 'Perhaps we have a misunderstanding...it\'s not Monday.  If you really do need to send this in now, please contact the Assistants at 303-929-1845.  Love, Winston');
 		}
 
 		//Get what phone texted at us, along with their message.
@@ -190,11 +212,9 @@ n.on('end', function(){
 		//message.  We will mail the sender a message saying
 		//that this is an automated email account and that
 		//it didn't understand what it was saying.
-
-		var mailer = require('./helpers/mailer.js');
 		
 		mailer.mail(mail.from[0].address, 'Automated Reply to Your Message', 
-			'This email account is does not accept reply messages.  If you are getting emails from this account and would like them to stop, please call (720)-675-7146.\n\nLove,\nWinston.', 
+			'This email account does not accept reply messages.  If you are getting emails from this account and would like them to stop, please call (720)-675-7146.\n\nLove,\nWinston.', 
 			function(err, info){
 			if(err){
 				console.log('Error while replying to non-text email: '+err);
